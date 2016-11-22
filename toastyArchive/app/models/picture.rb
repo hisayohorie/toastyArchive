8c4607b4b3158a3b7cec9cc16cadf594dc6adae1
hisayohorie <hisayohorie@gmail.com>
2016-11-22 13:24:50 -0500

    created Picture model in database

---
+++ toastyArchive/app/models/picture.rb
@@ -0,0 +1,11 @@
+class Picture < ActiveRecord::Base
+  def change
+    create_table :pictures do |t|
+      t.date :date
+      t.string :title
+      t.string :url
+
+      t.timestamps null :false
+    end
+  end
+end
